f23e0886941ad19a5f5582e4866c40d10a332ebe
George <georgegaibor@gmail.com>
2022-04-07 16:01:10 -0500

    feat: navbar layout and style finished

--- adminpanel/src/components/navbar/NavBar.jsx
+++ adminpanel/src/components/navbar/NavBar.jsx
@@ -1,8 +1,51 @@
 import "./navbar.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search" />
+          <SearchIcon className="icon"/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon"/>
+            EN
+          </div>
+          <div className="item">
+            <DarkModeIcon className="icon"/>
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon"/>
+          </div>
+          <div className="item">
+            <NotificationsNoneIcon className="icon"/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon className="icon"/>
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon"/>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" 
+              alt="" 
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- adminpanel/src/pages/home/Home.jsx
+++ adminpanel/src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
+import NavBar from "../../components/navbar/NavBar"
 import SideBar from "../../components/sidebar/SideBar"
+
 import "./home.scss"
 
 const Home = () => {
   return (
     <div className="home">
         <SideBar/>
-        <div className="homeContainer">container</div>
+        <div className="homeContainer">
+          <NavBar/>
+          homeContainer
+        </div>
     </div>
   )
 }
